8c98350784a7b4f50a91c37bbf71d29f8263fd15
HabibaSabrina <habibasabrina92@gmail.com>
2023-03-31 05:25:43 +0600

    author info and bookmarked button is added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faBookmark } from '@fortawesome/free-regular-svg-icons'
+import { faBookmark } from '@fortawesome/free-regular-svg-icons'
 
 const Blog = (props) => {
     const { blogCoverImg, AuthorImg, authorName, blogTitle, readTime, publishDate } = props.blog
     return (
         <div>
             <img className='h-80 w-full rounded-xl' src={blogCoverImg} alt="" />
-            <div className='flex'>
-                <img className='w-12 rounded-full mr-5' src={AuthorImg} alt="" />
-                <div>
-                    <p className='font-semibold'>{authorName}</p>
-                    <p>{publishDate}</p>
+            <div className='flex justify-between my-8'>
+                <div className='flex'>
+                    <img className='w-12 rounded-full mr-5' src={AuthorImg} alt="" />
+                    <div>
+                        <p className='font-semibold'>{authorName}</p>
+                        <p>{publishDate}</p>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <p>{readTime} min read</p>
-                <button className='bg-white'><FontAwesomeIcon icon={faBookmark} /></button>
+                <div className='flex items-center gap-4'>
+                    <p>{readTime} min read</p>
+                    <button className=''><FontAwesomeIcon icon={faBookmark} /></button>
+                </div>
             </div>
         </div>
     );
