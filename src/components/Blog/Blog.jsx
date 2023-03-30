4ead2160ae26f584390cfa3ac5afcd9c9f7cf525
HabibaSabrina <habibasabrina92@gmail.com>
2023-03-31 05:14:00 +0600

    font awesome is installed

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faBookmark } from '@fortawesome/free-regular-svg-icons'
 
 const Blog = (props) => {
-    const {blogCoverImg, AuthorImg, authorName, blogTitle, readTime, publishDate} = props.blog
+    const { blogCoverImg, AuthorImg, authorName, blogTitle, readTime, publishDate } = props.blog
     return (
         <div>
             <img className='h-80 w-full rounded-xl' src={blogCoverImg} alt="" />
+            <div className='flex'>
+                <img className='w-12 rounded-full mr-5' src={AuthorImg} alt="" />
+                <div>
+                    <p className='font-semibold'>{authorName}</p>
+                    <p>{publishDate}</p>
+                </div>
+            </div>
+            <div>
+                <p>{readTime} min read</p>
+                <button className='bg-white'><FontAwesomeIcon icon={faBookmark} /></button>
+            </div>
         </div>
     );
 };
