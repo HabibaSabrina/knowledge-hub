1f6d5e82869d81ab9c182181331f6dfd22231b77
HabibaSabrina <habibasabrina92@gmail.com>
2023-04-01 17:42:59 +0600

    user image fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <div className=' border-b-2 border-slate-200'>
             <div className='flex justify-between items-center m-5'>
                 <h3 className='font-bold text-2xl'>Knowledge Cafe</h3>
-                <img className='w-12' src="/public/user.png" alt="" />
+                <img className='w-12' src="/user.png" alt="" />
             </div>
         </div>
     );
